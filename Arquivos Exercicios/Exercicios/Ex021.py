685b99c1ea7b8c41252f807b3c845d07bd122bba
Ronaldo Edicassio de Siqueira <75760249+ronaldoedicassio@users.noreply.github.com>
2021-01-29 12:34:01 -0300

    atualização pycharm e exercicio 21
    Interpretador estava dando erro, foi preciso re-instalar para voltar a funcionar

--- Arquivos Exercicios/Exercicios/Ex021.py
+++ Arquivos Exercicios/Exercicios/Ex021.py
@@ -1,10 +1,9 @@
 #Faça um programa para reproduzir um audio de um aquivo MP3
 
 import pygame
-pygame.init()
+pygame.mixer.init()
 pygame.mixer.music.load('Ex021.mp3')
 pygame.mixer.music.play()
-pygame.event.wait()
-
+while(pygame.mixer.music.get_busy()): pass
 
 
