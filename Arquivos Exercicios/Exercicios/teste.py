63fd77d968ff44394637c513b9e3188319ccd8e6
Ronaldo Edicassio de Siqueira <75760249+ronaldoedicassio@users.noreply.github.com>
2021-03-01 13:46:12 -0300

    update

--- Arquivos Exercicios/Exercicios/teste.py
+++ Arquivos Exercicios/Exercicios/teste.py
@@ -1,2 +1,11 @@
-letras = 8,2,3,2,4,6
-print(sorted(letras))
+#f = c * 9/5
+
+valoretanol = float (input("Entre com valor em Etanol ?"))
+valorgas = float (input("Entre com valor em Gasolina ?"))
+
+if ( valoretanol/valorgas < 0.70):
+    print("Melhor opção e abastecer com gasolina")
+else:
+    print("Abastecer com Etanol")
+
+#print("O Valor em fahrenheit é: {:.2f} °F".format(temperatura*(9/5) + 32))
